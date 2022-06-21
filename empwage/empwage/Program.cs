18c91f9a1c19f19e622b1b11c795dee5dc878527
Pratikbhandurge2022 <pratikbhandurge2222@gmail.com>
2022-06-21 14:12:32 +0530

    initial commit

---
+++ empwage/empwage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace empwage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
